fbc76f2902358ec0b5b05180a24071c5afb99219
DanielDonato <daniel_donato10@yahoo.com.br>
2019-08-29 23:46:43 -0300

    Interface de login e cadastro

--- app/src/main/java/com/danieldonato/instagram/activity/MainActivity.java
+++ app/src/main/java/com/danieldonato/instagram/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.danieldonato.instagram;
+package com.danieldonato.instagram.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.danieldonato.instagram.R;
 
 public class MainActivity extends AppCompatActivity {
 
