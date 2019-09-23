f608f2cda28181d2405cd51ffc800574063f730d
DanielDonato <daniel_donato10@yahoo.com.br>
2019-09-22 21:07:45 -0300

    Finalizando main activity

--- app/src/main/java/com/danieldonato/instagram/activity/MainActivity.java
+++ app/src/main/java/com/danieldonato/instagram/activity/MainActivity.java
@@ -1,15 +1,119 @@
 package com.danieldonato.instagram.activity;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.danieldonato.instagram.R;
+import com.danieldonato.instagram.fragment.FeedFragment;
+import com.danieldonato.instagram.fragment.PerfilFragment;
+import com.danieldonato.instagram.fragment.PesquisaFragment;
+import com.danieldonato.instagram.fragment.PostagemFragment;
+import com.danieldonato.instagram.helper.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Instagram");
+        setSupportActionBar(toolbar);
+        autenticacao = ConfiguracaoFirebase.getRefenciaAutenticacao();
+
+        //configuração do bottom navigation view ex
+        configuraBottomNavigationView();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.viewPager, new FeedFragment()).commit();
     }
+
+    private void configuraBottomNavigationView(){
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavigation);
+        bottomNavigationViewEx.enableAnimation(true);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
+
+        habilitarNavegacao(bottomNavigationViewEx);
+
+        /*
+        //configura item selecionado inicialmente
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true)
+        */
+    }
+
+    private void habilitarNavegacao(BottomNavigationViewEx viewEx){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        viewEx.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.ic_home:
+                        fragmentTransaction.replace(R.id.viewPager, new FeedFragment()).commit();
+                        return true;
+                    case R.id.ic_pesquisa:
+                        fragmentTransaction.replace(R.id.viewPager, new PesquisaFragment()).commit();
+                        return true;
+                    case R.id.ic_postagem:
+                        fragmentTransaction.replace(R.id.viewPager, new PostagemFragment()).commit();
+                        return true;
+                    case R.id.ic_perfil:
+                        fragmentTransaction.replace(R.id.viewPager, new PerfilFragment()).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_sair:
+                deslogarUsuario();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deslogarUsuario(){
+        try{
+            autenticacao.signOut();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
