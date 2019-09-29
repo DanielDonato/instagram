7ee54c7df82b7e65d271108509653602e9ac0816
DanielDonato <daniel_donato10@yahoo.com.br>
2019-09-29 20:49:09 -0300

    Tela de editar dados de perfil

---
+++ app/src/main/java/com/danieldonato/instagram/activity/EditarPerfilActivity.java
@@ -0,0 +1,22 @@
+package com.danieldonato.instagram.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.danieldonato.instagram.R;
+
+public class EditarPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_perfil);
+
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Editar Perfil");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_fechar_preto_24dp);
+    }
+}
