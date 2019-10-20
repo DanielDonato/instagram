b73e2b04187933b06fd023642647389110ddab24
DanielDonato <daniel_donato10@yahoo.com.br>
2019-10-20 19:06:34 -0300

    Finalizando tela de filtro

--- app/src/main/java/com/danieldonato/instagram/activity/FiltroActivity.java
+++ app/src/main/java/com/danieldonato/instagram/activity/FiltroActivity.java
@@ -4,28 +4,143 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 
 import com.danieldonato.instagram.R;
+import com.danieldonato.instagram.adapter.AdapterMiniaturas;
+import com.danieldonato.instagram.helper.RecyclerItemClickListener;
+import com.zomato.photofilters.FilterPack;
+import com.zomato.photofilters.imageprocessors.Filter;
+import com.zomato.photofilters.utils.ThumbnailItem;
+import com.zomato.photofilters.utils.ThumbnailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FiltroActivity extends AppCompatActivity {
 
+    static
+    {
+        System.loadLibrary("NativeImageProcessor");
+    }
+
     private ImageView imageFotoEscolhida;
     private Bitmap imagem;
+    private Bitmap imagemFiltro;
+    private List<ThumbnailItem> listaFiltros;
+
+    private RecyclerView recyclerFiltros;
+    private AdapterMiniaturas adapterMiniaturas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtro);
 
+        listaFiltros = new ArrayList<>();
+
         imageFotoEscolhida = findViewById(R.id.imageFotoEscolhida);
+        recyclerFiltros = findViewById(R.id.recyclerFiltros);
+
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Filtros");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_fechar_preto_24dp);
 
         Bundle bundle = getIntent().getExtras();
         if(bundle != null){
             byte[] dadosImagem = bundle.getByteArray("fotoEscolhida");
             imagem = BitmapFactory.decodeByteArray(dadosImagem, 0, dadosImagem.length);
             imageFotoEscolhida.setImageBitmap(imagem);
+
+            adapterMiniaturas = new AdapterMiniaturas(listaFiltros, getApplicationContext());
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this,
+                    LinearLayoutManager.HORIZONTAL, false);
+            recyclerFiltros.setLayoutManager(layoutManager);
+            recyclerFiltros.setAdapter(adapterMiniaturas);
+
+            recyclerFiltros.addOnItemTouchListener(new RecyclerItemClickListener(
+                    getApplicationContext(),
+                    recyclerFiltros,
+                    new RecyclerItemClickListener.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(View view, int position) {
+                            ThumbnailItem item = listaFiltros.get(position);
+                            imagemFiltro = imagem.copy(imagem.getConfig(), true);
+                            Filter filter = item.filter;
+                            imageFotoEscolhida.setImageBitmap(filter.processFilter(imagemFiltro));
+                        }
+
+                        @Override
+                        public void onLongItemClick(View view, int position) {
+
+                        }
+
+                        @Override
+                        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                        }
+                    }
+            ));
+
+            recuperarFiltros();
+        }
+    }
+
+    private void recuperarFiltros(){
+
+        listaFiltros.clear();
+        ThumbnailsManager.clearThumbs();
+
+        ThumbnailItem item = new ThumbnailItem();
+        item.image = imagem;
+        item.filterName = "Normal";
+        ThumbnailsManager.addThumb(item);
+
+        List<Filter> filtros = FilterPack.getFilterPack(getApplicationContext());
+        for (Filter filtro : filtros){
+            ThumbnailItem itemFiltro = new ThumbnailItem();
+            itemFiltro.image = imagem;
+            itemFiltro.filter = filtro;
+            itemFiltro.filterName = filtro.getName();
+            ThumbnailsManager.addThumb(itemFiltro);
         }
 
+        listaFiltros.addAll(ThumbnailsManager.processThumbs(getApplicationContext()));
+        adapterMiniaturas.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_filtro, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.ic_salvar_postagem:
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return false;
     }
 }
