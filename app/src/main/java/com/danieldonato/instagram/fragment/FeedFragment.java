7d912bd9fb2d4a797e04616288166d470c825803
DanielDonato <daniel_donato10@yahoo.com.br>
2019-11-02 19:31:56 -0300

    Recuperando feed

--- app/src/main/java/com/danieldonato/instagram/fragment/FeedFragment.java
+++ app/src/main/java/com/danieldonato/instagram/fragment/FeedFragment.java
@@ -2,18 +2,39 @@ package com.danieldonato.instagram.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.danieldonato.instagram.R;
+import com.danieldonato.instagram.adapter.AdapterFeed;
+import com.danieldonato.instagram.helper.ConfiguracaoFirebase;
+import com.danieldonato.instagram.helper.UsuarioFirebase;
+import com.danieldonato.instagram.model.Feed;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FeedFragment extends Fragment {
 
+    private RecyclerView recyclerFeed;
+    private AdapterFeed adapterFeed;
+    private List<Feed> listaFeed = new ArrayList<>();
+    private ValueEventListener valueEventListenerFeed;
+    private DatabaseReference feedRef;
+    private String idUsuarioLogado;
 
     public FeedFragment() {
         // Required empty public constructor
@@ -24,7 +45,50 @@ public class FeedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+        View view = inflater.inflate(R.layout.fragment_feed, container, false);
+
+        idUsuarioLogado = UsuarioFirebase.getIdentificadoUsuario();
+        feedRef = ConfiguracaoFirebase.getReferenceFirebase()
+                .child("feed")
+                .child(idUsuarioLogado);
+
+        recyclerFeed = view.findViewById(R.id.recyclerFeed);
+        recyclerFeed.setHasFixedSize(true);
+        recyclerFeed.setLayoutManager(
+                new LinearLayoutManager(getActivity())
+        );
+        adapterFeed = new AdapterFeed(listaFeed, getActivity());
+        recyclerFeed.setAdapter(adapterFeed);
+        return view;
     }
 
+    private void listarFeed(){
+        valueEventListenerFeed = feedRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds : dataSnapshot.getChildren()){
+                    listaFeed.add(ds.getValue(Feed.class));
+                }
+                Collections.reverse(listaFeed);
+                adapterFeed.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        listarFeed();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        feedRef.removeEventListener(valueEventListenerFeed);
+    }
 }
